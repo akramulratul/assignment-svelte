49660efca61f7feb7c803632c30fe949c561f466
Akramul Ratul <akramulratul@gmail.com>
2023-09-02 20:07:26 +0600

    Environment added with vite & tailwind

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import TailwindCss from './lib/TailwindCSS.svelte';
+  import Layout from './Component/Layout.svelte'
+</script>
+<TailwindCss/>
+<Layout/>
+<main>
+ 
+</main>
+
+<style>
+
+</style>
---
+++ src/Component/Layout.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+
+
+    <div class="grid grid-cols-12 grid-rows-6">
+    <!-- For smaller screens, it's a 6x4 grid, so you'll have 4 columns. For medium (`md:`) screens and up, it'll be 6x12, so 12 columns. Adjust the `gap-4` (which sets both row and column gaps) to your preference. -->
+  <div class="col-start-1 grid-rows-3 grid-flow-col col-span-2 bg-blue-400">1</div>
+  <div class="col-start-3 col-span-4 bg-rose-100">2</div>
+  <div class="col-start-7 col-span-6 bg-blue-400">3</div>
+  </div>
+<style>
+
+</style>
